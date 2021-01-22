bd1388816a19d63645b78b70c0c762ba84159800
Kody Gordon <gordon.km89@gmail.com>
2021-01-22 12:53:51 -0800

    Add HTML elements, CSS elements, and IMG

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
+//User Logic//
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    event.preventDefault();
 
-//Business Logic//
-function robogerTranslate(number) {
-  let output = [];
-  for (i = 0; i <= number; i++) {
-    if (i.toString().includes(3)) {
-      output.push("Wont you be my neighbor?");
-    } else if (i.toString().includes(2)) {
-      output.push("boop!")
-    } else if (i.toString().includes(1)) {
-      output.push("Beep!")
-    } else {
-      output.push(i);
-    }
+    let userInput = $("#numberVal").val();
+    let newArray = robogerTranslate(userInput);
 
-  }
-  return output;
+    let joinedArray = [];
+    joinedArray.push(newArray.join(" "));
+
+    $(".speech-bubble").show();
+    $("#result").text(joinedArray);
+
+    $("#result").show();
+    
+//Business Logic
+    function robogerTranslate(number) {
+      let output = [];
+      for (i = 0; i <= number; i++) {
+        if (i.toString().includes(3)) {
+          output.push("Wont you be my neighbor?");
+        } else if (i.toString().includes(2)) {
+          output.push("boop!")
+        } else if (i.toString().includes(1)) {
+          output.push("Beep!")
+        } else {
+          output.push(i);
+        }
+
+      }
+      return output;
+    };
+
+  });
+});
