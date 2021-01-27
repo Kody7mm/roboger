fd86b2bd74a9f5056b9e8f8691ec6621560c4406
Kody Gordon <35934364+Kody7mm@users.noreply.github.com>
2021-01-26 18:46:15 -0800

    Update scripts.js
    Small edit to JS for separation of logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,23 @@ $(document).ready(function () {
     $("#result").text(joinedArray);
 
     $("#result").show();
+  });
+});
 
 //Business Logic
-    function robogerTranslate(number) {
-      let output = [];
-      for (i = 0; i <= number; i++) {
-        if (i.toString().includes(3)) {
-          output.push("Wont you be my neighbor?");
-        } else if (i.toString().includes(2)) {
-          output.push("boop!")
-        } else if (i.toString().includes(1)) {
-          output.push("Beep!")
-        } else {
-          output.push(i);
-        }
+function robogerTranslate(number) {
+  let output = [];
+  for (i = 0; i <= number; i++) {
+    if (i.toString().includes(3)) {
+      output.push("Wont you be my neighbor?");
+    } else if (i.toString().includes(2)) {
+      output.push("boop!")
+    } else if (i.toString().includes(1)) {
+      output.push("Beep!")
+    } else {
+      output.push(i);
+    }
 
-      }
-      return output;
-    };
-});
+  }
+  return output;
+};
