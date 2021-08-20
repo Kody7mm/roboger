f9a7c2bdaa0ac130874f25518572c3914f652d17
kody7mm <gordon.km89@gmail.com>
2021-08-20 09:48:29 -0700

    Add JS business and UI logic

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+//Business Logic
+function robotTranslate(number) {
+  let output = [];
+  for (i = 0; i <= number; i++) {
+    if (i.toString().includes(3)) {
+      output.push("Wont you be my neighbor?");
+    } else if (i.toString().includes(2)) {
+      output.push("boop!")
+    } else if (i.toString().includes(1)) {
+      output.push("Beep!")
+    } else {
+      output.push(i);
+    }
+
+  }
+  return output;
+};
+
+//User Logic//
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    event.preventDefault();
+
+    let userInput = $("#numberVal").val();
+    let newArray = robotTranslate(userInput);
+
+    let joinedArray = [];
+    joinedArray.push(newArray.join(" "));
+
+    $(".speech-bubble").show();
+    $("#result").text(joinedArray);
+
+    $("#result").show();
+  });
+});
