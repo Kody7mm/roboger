bc8cd4aefaa380bad0ec53a5e79eb443936b9b80
kody7mm <gordon.km89@gmail.com>
2021-08-20 14:40:19 -0700

    Add fun easter eggs to project

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@
 function robotTranslate(number) {
   let output = [];
   for (i = 0; i <= number; i++) {
-    if (i.toString().includes(3)) {
-      output.push("Wont you be my neighbor?");
+    if (i.toString().includes(69)) {
+      output.push("I see what you did here...");
+    } else if (i.toString().includes(42)) {
+      output.push("Answer to the Ultimate Question of Life, the Universe and Everything!")
+    } else if (i.toString().includes(3)) {
+      output.push("Wont you be my neighbor?!")
     } else if (i.toString().includes(2)) {
       output.push("boop!")
     } else if (i.toString().includes(1)) {
